04c5b96c68d78322eb393a3d448814442cdd49d2
sabeeh1123 <sabeehmansuri07@gmail.com>
2026-06-15 12:44:09 +0530

    chore: update service worker zone ID
    Update the zoneId in the service worker configuration to ensure correct integration with the ad network.

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 11150244
+    "zoneId": 11150323
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
